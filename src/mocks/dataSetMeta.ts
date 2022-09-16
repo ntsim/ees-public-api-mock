151b69572184fcfd378caef4b08d9f3c7e19922e
Nicholas Tsim <nicholastsim@gmail.com>
2022-09-16 18:17:29 +0100

    Remove nested groupings in filters/indicators

--- src/mocks/dataSetMeta.ts
+++ src/mocks/dataSetMeta.ts
@@ -36,31 +36,19 @@ export const absenceRatesDataSetMeta: DataSetMetaViewModel = {
       totalId: '14cbe6d4-e0ff-47e7-ade2-3af43813165a',
       options: [
         {
-          id: '2a006856-76c0-48c1-97c3-bc0e1318451f',
-          label: 'Default',
-          options: [
-            {
-              id: '14cbe6d4-e0ff-47e7-ade2-3af43813165a',
-              label: 'Total',
-            },
-          ],
+          id: '14cbe6d4-e0ff-47e7-ade2-3af43813165a',
+          label: 'Total',
         },
       ],
     },
   ],
   indicators: [
     {
-      id: '6c27cad1-abc7-4172-a949-185f06626bb0',
-      label: 'Headline Absence fields',
-      options: [
-        {
-          id: 'b38edd21-892e-4793-3361-08da0807f3aa',
-          label: 'Number of overall absence sessions',
-          name: 'sess_overall',
-          unit: '',
-          decimalPlaces: 0,
-        },
-      ],
+      id: 'b38edd21-892e-4793-3361-08da0807f3aa',
+      label: 'Number of overall absence sessions',
+      name: 'sess_overall',
+      unit: '',
+      decimalPlaces: 0,
     },
   ],
   locations: [englandLocationMeta],
@@ -79,18 +67,12 @@ export const absenceRatesByCharacteristicsDataSetMeta: DataSetMetaViewModel = {
       totalId: '850092de-e989-4818-a8ea-4be2fb5d8bb4',
       options: [
         {
-          id: '96e9a1e4-8eae-4a28-8d85-c4133722d72a',
-          label: 'Default',
-          options: [
-            {
-              id: '0d194014-8d2a-4abc-8b1f-e7ae899e8eb3',
-              label: 'Total',
-            },
-            {
-              id: 'b936e2a5-a082-443c-a2ce-2c24195ad664',
-              label: 'State-funded primary',
-            },
-          ],
+          id: '0d194014-8d2a-4abc-8b1f-e7ae899e8eb3',
+          label: 'Total',
+        },
+        {
+          id: 'b936e2a5-a082-443c-a2ce-2c24195ad664',
+          label: 'State-funded primary',
         },
       ],
     },
@@ -101,46 +83,34 @@ export const absenceRatesByCharacteristicsDataSetMeta: DataSetMetaViewModel = {
       totalId: '1df4d473-0a9f-4171-9194-8b5ed747f87d',
       options: [
         {
-          id: '8ab5e0c9-bf16-4cd6-8a5d-c2be2f1369ec',
-          label: 'Default',
-          options: [
-            {
-              id: '1df4d473-0a9f-4171-9194-8b5ed747f87d',
-              label: 'Total',
-            },
-            {
-              id: '9b81021a-f60f-4d38-8816-dd3aef54de1b',
-              label: 'Ethnicity Major Asian Total',
-            },
-            {
-              id: '1020595a-308e-484c-8d39-a78c83734fe9',
-              label: 'Ethnicity Major Black Total',
-            },
-          ],
+          id: '1df4d473-0a9f-4171-9194-8b5ed747f87d',
+          label: 'Total',
+        },
+        {
+          id: '9b81021a-f60f-4d38-8816-dd3aef54de1b',
+          label: 'Ethnicity Major Asian Total',
+        },
+        {
+          id: '1020595a-308e-484c-8d39-a78c83734fe9',
+          label: 'Ethnicity Major Black Total',
         },
       ],
     },
   ],
   indicators: [
     {
-      id: 'dd45db15-618d-4de2-a601-73769c207946',
-      label: 'Headline Absence fields',
-      options: [
-        {
-          id: '772c60cd-d8eb-40c8-5ae9-08da0b95637f',
-          label: 'Number of authorised absence sessions',
-          name: 'sess_authorised',
-          unit: '',
-          decimalPlaces: 0,
-        },
-        {
-          id: 'c82070d0-d1fd-4ac2-5ae8-08da0b95637f',
-          label: 'Number of overall absence sessions',
-          name: 'sess_overall',
-          unit: '',
-          decimalPlaces: 0,
-        },
-      ],
+      id: '772c60cd-d8eb-40c8-5ae9-08da0b95637f',
+      label: 'Number of authorised absence sessions',
+      name: 'sess_authorised',
+      unit: '',
+      decimalPlaces: 0,
+    },
+    {
+      id: 'c82070d0-d1fd-4ac2-5ae8-08da0b95637f',
+      label: 'Number of overall absence sessions',
+      name: 'sess_overall',
+      unit: '',
+      decimalPlaces: 0,
     },
   ],
   locations: countryRegionalLocationMeta,
@@ -159,38 +129,26 @@ export const permanentExclusionsDataSetMeta: DataSetMetaViewModel = {
       totalId: '49557959-21e3-4acd-97e8-961259bbb7ee',
       options: [
         {
-          id: '00a22faa-588a-4b4e-9c0b-3174b09f3fa9',
-          label: 'Default',
-          options: [
-            {
-              id: '49557959-21e3-4acd-97e8-961259bbb7ee',
-              label: 'Total',
-            },
-          ],
+          id: '49557959-21e3-4acd-97e8-961259bbb7ee',
+          label: 'Total',
         },
       ],
     },
   ],
   indicators: [
     {
-      id: '0d534e47-2e6a-4cc7-9213-e0ce5298195e',
-      label: 'Default',
-      options: [
-        {
-          id: '04a90197-d94f-4606-5b0b-08da6fdcfe48',
-          label: 'Permanent exclusions (rate)',
-          name: 'perm_excl_rate',
-          unit: '%',
-          decimalPlaces: 2,
-        },
-        {
-          id: '09f59b22-1275-4922-5b0d-08da6fdcfe48',
-          label: 'Suspension (rate)',
-          name: 'susp_rate',
-          unit: '%',
-          decimalPlaces: 2,
-        },
-      ],
+      id: '04a90197-d94f-4606-5b0b-08da6fdcfe48',
+      label: 'Permanent exclusions (rate)',
+      name: 'perm_excl_rate',
+      unit: '%',
+      decimalPlaces: 2,
+    },
+    {
+      id: '09f59b22-1275-4922-5b0d-08da6fdcfe48',
+      label: 'Suspension (rate)',
+      name: 'susp_rate',
+      unit: '%',
+      decimalPlaces: 2,
     },
   ],
   locations: [englandLocationMeta],
