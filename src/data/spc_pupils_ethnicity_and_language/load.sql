3877b26ddfa8368d6d9e1eb4197716132e76baeb
Nicholas Tsim <nicholastsim@gmail.com>
2022-11-06 22:56:46 +0000

    Re-import SPC datasets

--- src/data/spc_pupils_ethnicity_and_language/load.sql
+++ src/data/spc_pupils_ethnicity_and_language/load.sql
@@ -3,3 +3,4 @@ COPY time_periods FROM '/home/nick/Development/personal/ees-public-api-mock/src/
 COPY locations FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/spc_pupils_ethnicity_and_language/locations.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
 COPY filters FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/spc_pupils_ethnicity_and_language/filters.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
 COPY indicators FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/spc_pupils_ethnicity_and_language/indicators.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
+COPY data_facts FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/spc_pupils_ethnicity_and_language/data_facts.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
