1db144978c073e065a5d5b9ec97cb55c40cdc1a0
Nicholas Tsim <nicholastsim@gmail.com>
2022-11-09 15:45:51 +0000

    Reduce hashed id min length to 8

--- src/utils/idHashers.ts
+++ src/utils/idHashers.ts
@@ -1,13 +1,13 @@
 import Hashids from 'hashids';
 
 export function createLocationIdHasher(): Hashids {
-  return new Hashids('locations', 16);
+  return new Hashids('locations', 8);
 }
 
 export function createFilterIdHasher(): Hashids {
-  return new Hashids('filters', 16);
+  return new Hashids('filters', 8);
 }
 
 export function createIndicatorIdHasher(): Hashids {
-  return new Hashids('indicators', 16);
+  return new Hashids('indicators', 8);
 }
